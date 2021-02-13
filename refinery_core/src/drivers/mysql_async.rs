d7948e368131bb76363b3e62619d55066c448df7
rykario <keeganmyers608@gmail.com>
2021-02-12 18:45:13 -0700

    fixed type errors

--- refinery_core/src/drivers/mysql_async.rs
+++ refinery_core/src/drivers/mysql_async.rs
@@ -3,17 +3,16 @@ use crate::Migration;
 use async_trait::async_trait;
 use chrono::{DateTime, Local};
 use mysql_async::{
-    error::Error as MError, prelude::Queryable, Conn, IsolationLevel, Pool,
-    Transaction as MTransaction, TransactionOptions,
+    prelude::Queryable, Error as MError, IsolationLevel, Pool, Transaction as MTransaction, TxOpts,
 };
 
-async fn query_applied_migrations(
-    transaction: MTransaction<Conn>,
+async fn query_applied_migrations<'a>(
+    transaction: &mut MTransaction<'a>,
     query: &str,
-) -> Result<(MTransaction<Conn>, Vec<Migration>), MError> {
-    let result = transaction.query(query).await?;
+) -> Result<Vec<Migration>, MError> {
+    let result = transaction.query_iter(query).await?;
 
-    let (transaction, applied) = result
+    let applied = result
         .map_and_drop(|row| {
             let (version, name, applied_on, checksum): (i32, String, String, String) =
                 mysql_async::from_row(row);
@@ -32,7 +31,7 @@ async fn query_applied_migrations(
         })
         .await?;
 
-    Ok((transaction, applied))
+    Ok(applied)
 }
 
 #[async_trait]
@@ -40,14 +39,14 @@ impl AsyncTransaction for Pool {
     type Error = MError;
 
     async fn execute(&mut self, queries: &[&str]) -> Result<usize, Self::Error> {
-        let conn = self.get_conn().await?;
-        let mut options = TransactionOptions::new();
-        options.set_isolation_level(Some(IsolationLevel::ReadCommitted));
+        let mut conn = self.get_conn().await?;
+        let mut options = TxOpts::new();
+        options.with_isolation_level(Some(IsolationLevel::ReadCommitted));
 
         let mut transaction = conn.start_transaction(options).await?;
         let mut count = 0;
         for query in queries {
-            transaction = transaction.query(query).await?.drop_result().await?;
+            transaction.query_iter(query).await?.drop_result().await?;
             count += 1;
         }
         transaction.commit().await?;
@@ -61,12 +60,12 @@ impl AsyncQuery<Vec<Migration>> for Pool {
         &mut self,
         query: &str,
     ) -> Result<Vec<Migration>, <Self as AsyncTransaction>::Error> {
-        let conn = self.get_conn().await?;
-        let mut options = TransactionOptions::new();
-        options.set_isolation_level(Some(IsolationLevel::ReadCommitted));
-        let transaction = conn.start_transaction(options).await?;
+        let mut conn = self.get_conn().await?;
+        let mut options = TxOpts::new();
+        options.with_isolation_level(Some(IsolationLevel::ReadCommitted));
+        let mut transaction = conn.start_transaction(options).await?;
 
-        let (transaction, applied) = query_applied_migrations(transaction, query).await?;
+        let applied = query_applied_migrations(&mut transaction, query).await?;
         transaction.commit().await?;
         Ok(applied)
     }
